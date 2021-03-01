4e33a74e2b0f3ba1745ae94276b01d7f78eab945
Joan Andrés <andres6936@live.com>
2021-03-01 18:30:58 -0500

    Use of ResumeCardProgress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 
-import {Card} from './Component/Card';
-import {CardBody} from "./Component/CardBody";
 import {CalendarIcon} from "./Icon/Calendar";
 import {ResumeCard} from "./Object/ResumeCard";
 import {BellIcon} from "./Icon/Bell";
 import {BookIcon} from "./Icon/Book";
 import {ChatIcon} from "./Icon/Chat";
+import {ResumeCardProgress} from "./Object/ResumeCardProgress";
 
 function App() {
     return (
@@ -15,33 +14,8 @@ function App() {
 
             <ResumeCard title={"EARNINGS (MONTHLY)"} text={"$40,000"} icon={<CalendarIcon/>} color={"text-primary"}/>
             <ResumeCard title={"EARNINGS (ANNUAL)"} text={"$215,000"} icon={<BellIcon/>} color={"text-success"}/>
-
-            <Card>
-                <CardBody>
-                    <div className={"row"}>
-                        <div className={"col-9"}>
-                            <p className={"small fw-bold text-info mb-0"}>TASKS</p>
-                            <div className={"row"}>
-                                <div className={"col-3"}>
-                                    <p className={"small fw-bold mb-0"}>50%</p>
-                                </div>
-                                <div className={"col-9"}>
-                                    <div className="progress">
-                                        <div className="progress-bar bg-info" role="progressbar" style={{width: '50%'}}
-                                             aria-valuenow="25"
-                                             aria-valuemin="0" aria-valuemax="100">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={"col-3"}>
-                            <BookIcon/>
-                        </div>
-                    </div>
-                </CardBody>
-            </Card>
-
+            <ResumeCardProgress title={"TASKS"} percentage={50} icon={<BookIcon/>} colorTitle={"text-info"}
+                                colorBar={"bg-info"}/>
             <ResumeCard title={"PENDING REQUESTS"} text={"18"} icon={<ChatIcon/>} color={"text-warning"}/>
         </div>
     );
---
+++ src/Object/ResumeCardProgress.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {Row} from "../Component/Row";
+import {Card} from "../Component/Card";
+import {CardBody} from "../Component/CardBody";
+
+export class ResumeCardProgress extends React.Component {
+    render() {
+        return <Card>
+            <CardBody>
+                <Row>
+                    <div className={"col-9"}>
+                        <p className={"small fw-bold mb-0 " + this.props.colorTitle}>{this.props.title}</p>
+                        <div className={"row"}>
+                            <div className={"col-3"}>
+                                <p className={"small fw-bold mb-0"}>{this.props.percentage}%</p>
+                            </div>
+                            <div className={"col-9"}>
+                                <div className="progress">
+                                    <div className={"progress-bar " + this.props.colorBar} role="progressbar"
+                                         style={{width: this.props.percentage + '%'}}
+                                         aria-valuenow="25"
+                                         aria-valuemin="0" aria-valuemax="100">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"col-3"}>
+                        {this.props.icon}
+                    </div>
+                </Row>
+            </CardBody>
+        </Card>
+    }
+}
