310a7bf7c291a7ad9c9f171e6f07e7aa1db94d98
Joan Andrés <andres6936@live.com>
2021-03-01 18:06:03 -0500

    Added new component {ResumeCard}

--- src/App.js
+++ src/App.js
@@ -4,43 +4,16 @@ import {Row} from './Component/Row';
 import {Card} from './Component/Card';
 import {CardBody} from "./Component/CardBody";
 import {CalendarIcon} from "./Icon/Calendar";
+import {ResumeCard} from "./Object/ResumeCard";
+import {BellIcon} from "./Icon/Bell";
 
 function App() {
     return (
         <div className="container-fluid bg-light">
             <h1 className={"text-muted py-3"}>Dashboard</h1>
 
-            <Card>
-                <CardBody>
-                    <Row>
-                        <div className={"col-9"}>
-                            <p className={"small fw-bold text-primary mb-0"}>EARNINGS (MONTHLY)</p>
-                            <p className={"small fw-bold mb-0"}>$40,000</p>
-                        </div>
-                        <div className={"col-3"}>
-                            <CalendarIcon/>
-                        </div>
-                    </Row>
-                </CardBody>
-            </Card>
-
-            <Card>
-                <CardBody>
-                    <div className={"row"}>
-                        <div className={"col-9"}>
-                            <p className={"small fw-bold text-success mb-0"}>EARNINGS (ANNUAL)</p>
-                            <p className={"small fw-bold mb-0"}>$215,000</p>
-                        </div>
-                        <div className={"col-3"}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                                 className="bi bi-bell text-muted" viewBox="0 0 16 16">
-                                <path
-                                    d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
-                            </svg>
-                        </div>
-                    </div>
-                </CardBody>
-            </Card>
+            <ResumeCard title={"EARNINGS (MONTHLY)"} text={"$40,000"} icon={<CalendarIcon/>} color={"text-primary"}/>
+            <ResumeCard title={"EARNINGS (ANNUAL)"} text={"$215,000"} icon={<BellIcon/>} color={"text-success"}/>
 
             <Card>
                 <CardBody>
---
+++ src/Object/ResumeCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {Row} from "../Component/Row";
+import {Card} from "../Component/Card";
+import {CardBody} from "../Component/CardBody";
+
+export class ResumeCard extends React.Component {
+    render() {
+        return <Card>
+            <CardBody>
+                <Row>
+                    <div className={"col-9"}>
+                        <p className={"small fw-bold mb-0 " + this.props.color}>{this.props.title}</p>
+                        <p className={"small fw-bold mb-0"}>{this.props.text}</p>
+                    </div>
+                    <div className={"col-3"}>
+                        {this.props.icon}
+                    </div>
+                </Row>
+            </CardBody>
+        </Card>
+    }
+}
