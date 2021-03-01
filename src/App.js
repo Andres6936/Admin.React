c3fd13dea573d91307954c822fc7e957d0f2244c
Joan Andrés <andres6936@live.com>
2021-03-01 17:55:05 -0500

    Added new component {Row}

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+import {Row} from './Component/Row';
 import {Card} from './Component/Card';
 import {CardBody} from "./Component/CardBody";
 
@@ -10,7 +11,7 @@ function App() {
 
             <Card>
                 <CardBody>
-                    <div className={"row"}>
+                    <Row>
                         <div className={"col-9"}>
                             <p className={"small fw-bold text-primary mb-0"}>EARNINGS (MONTHLY)</p>
                             <p className={"small fw-bold mb-0"}>$40,000</p>
@@ -22,7 +23,7 @@ function App() {
                                     d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
                             </svg>
                         </div>
-                    </div>
+                    </Row>
                 </CardBody>
             </Card>
 
