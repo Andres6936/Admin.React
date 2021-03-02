4988470b814df1bd7a621546b2bce0de0a309a4e
Joan Andrés <andres6936@live.com>
2021-03-02 08:30:50 -0500

    Added two news objects

---
+++ src/Object/RevenueSources.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Row} from "../Component/Row";
+import {Card} from "../Component/Card";
+import {CardBody} from "../Component/CardBody";
+
+export class RevenueSources extends React.Component {
+
+}
