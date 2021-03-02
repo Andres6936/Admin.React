ea67dc00330cbf6a71428097e567e671226731b0
Joan Andrés <andres6936@live.com>
2021-03-02 11:27:34 -0500

    Added the list element

--- src/App.js
+++ src/App.js
@@ -20,7 +20,15 @@ import {CardHeader} from "./Component/CardHeader";
 
 function App() {
     return <React.Fragment>
-        <nav className={"navbar-expand navbar-light bg-white"}>
+        <nav className={"navbar-expand navbar-light bg-white shadow"}>
+            <a className="nav-link float-start" aria-current="page" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     className="bi bi-list text-muted" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                          d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+            </a>
+
             <ul className={"nav my-3 justify-content-end"}>
                 <li className="nav-item">
                     <a className="nav-link" aria-current="page" href="#">
