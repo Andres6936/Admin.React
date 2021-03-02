617f55102a9e330c179abdb41dda74ef4f2c143f
Joan Andrés <andres6936@live.com>
2021-03-02 10:08:28 -0500

    Added new section {Development}

--- src/App.js
+++ src/App.js
@@ -97,6 +97,18 @@ function App() {
                     <a href={"https://undraw.co/"} className={"text-decoration-none"}>Browse Illustrations on unDraw</a>
                 </CardBody>
             </Card>
+
+            <Card>
+                <CardHeader title={"Development Approach"}/>
+                <CardBody>
+                    <p className={"text-muted"}>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order
+                        to reduce CSS bloat and
+                        poor page performance. Custom CSS classes are used to create custom components and custom
+                        utility classes.</p>
+                    <p className={"text-muted"}>Before working with this theme, you should become familiar with the
+                        Bootstrap framework, especially the utility classes.</p>
+                </CardBody>
+            </Card>
         </div>
     );
 }
