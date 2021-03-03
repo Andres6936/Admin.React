f019e904d2a04d064d3987a731fa4baed63837a1
Joan Andrés <andres6936@live.com>
2021-03-03 14:30:03 -0500

    Added documentation to method and classes

--- src/Layout/Sidebar.js
+++ src/Layout/Sidebar.js
@@ -9,6 +9,12 @@ import {GraphUpIcon} from "../Icon/GraphUp";
 import {TableIcon} from "../Icon/Table";
 import {ArrowLeftCircleFillIcon} from "../Icon/ArrowLeftCircleFill";
 
+/**
+ * @function setStatusSidebar {function(boolean) -> void} Set the state of
+ * sidebar, if the parameter send to method is true the state of sidebar is
+ * hidden, if the parameter send to method is false the state of sidebar is
+ * show, for default the state of sidebar is show.
+ */
 export class Sidebar extends React.Component {
     constructor(props) {
         super(props);
@@ -20,6 +26,12 @@ export class Sidebar extends React.Component {
         this.toggleSidebar = this.toggleSidebar.bind(this);
     }
 
+    /**
+     * Change the state of sidebar. hidden to show <-> show to hidden.
+     *
+     * @return {boolean} The state of sidebar, if is true the state of sidebar
+     * is hidden, if is false the state of sidebar is show.
+     */
     toggleSidebar() {
         let currentStateSidebar = this.state.hiddenSidebar;
         this.setState({hiddenSidebar: !currentStateSidebar})
