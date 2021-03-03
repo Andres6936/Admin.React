e695167d10980072f2bf2a25504c3b928721104b
Joan Andrés <andres6936@live.com>
2021-03-03 12:38:38 -0500

    Convert function to class

--- src/App.js
+++ src/App.js
@@ -16,52 +16,54 @@ import {Illustrations} from "./Object/Illustrations";
 import {DevelopmentApproach} from "./Object/DevelopmentApproach";
 import {Footer} from "./Layout/Footer";
 
-function App() {
-    return <div className={"row m-0 p-0"}>
-        <Sidebar/>
+export default class App extends React.Component {
+    render() {
+        return <div className={"row m-0 p-0"}>
+            <Sidebar/>
 
-        <div className={"col-9 p-0 m-0"}>
-            <Navigation/>
+            <div className={"col-9 p-0 m-0"}>
+                <Navigation/>
 
-            <div className="container-fluid bg-light pb-4">
-                <h1 className={"text-muted py-3"}>Dashboard</h1>
+                <div className="container-fluid bg-light pb-4">
+                    <h1 className={"text-muted py-3"}>Dashboard</h1>
 
-                <ResumeCard title={"EARNINGS (MONTHLY)"} text={"$40,000"} icon={<CalendarIcon/>}
-                            color={"text-primary"}/>
-                <ResumeCard title={"EARNINGS (ANNUAL)"} text={"$215,000"} icon={<BellIcon/>} color={"text-success"}/>
-                <ResumeCardProgress title={"TASKS"} percentage={50} icon={<BookIcon/>} colorTitle={"text-info"}
-                                    colorBar={"bg-info"}/>
-                <ResumeCard title={"PENDING REQUESTS"} text={"18"} icon={<ChatIcon/>} color={"text-warning"}/>
+                    <ResumeCard title={"EARNINGS (MONTHLY)"} text={"$40,000"} icon={<CalendarIcon/>}
+                                color={"text-primary"}/>
+                    <ResumeCard title={"EARNINGS (ANNUAL)"} text={"$215,000"} icon={<BellIcon/>}
+                                color={"text-success"}/>
+                    <ResumeCardProgress title={"TASKS"} percentage={50} icon={<BookIcon/>} colorTitle={"text-info"}
+                                        colorBar={"bg-info"}/>
+                    <ResumeCard title={"PENDING REQUESTS"} text={"18"} icon={<ChatIcon/>} color={"text-warning"}/>
 
-                <EarningOverview/>
-                <RevenueSources/>
-                <Projects/>
+                    <EarningOverview/>
+                    <RevenueSources/>
+                    <Projects/>
 
-                <ColorCard title={"Primary"} subtitle={"#4e73df"} backgroundColor={"bg-primary"}/>
-                <ColorCard title={"Success"} subtitle={"#4e73df"} backgroundColor={"bg-success"}/>
-                <ColorCard title={"Info"} subtitle={"#4e73df"} backgroundColor={"bg-info"}/>
-                <ColorCard title={"Warning"} subtitle={"#4e73df"} backgroundColor={"bg-warning"}/>
-                <ColorCard title={"Danger"} subtitle={"#4e73df"} backgroundColor={"bg-danger"}/>
-                <ColorCard title={"Secondary"} subtitle={"#4e73df"} backgroundColor={"bg-secondary"}/>
-                <ColorCard title={"Dark"} subtitle={"#4e73df"} backgroundColor={"bg-dark"}/>
-                <ColorCard title={"Light"} subtitle={"#4e73df"} backgroundColor={"bg-light"}
-                           foregroundColor={"text-muted"}/>
+                    <ColorCard title={"Primary"} subtitle={"#4e73df"} backgroundColor={"bg-primary"}/>
+                    <ColorCard title={"Success"} subtitle={"#4e73df"} backgroundColor={"bg-success"}/>
+                    <ColorCard title={"Info"} subtitle={"#4e73df"} backgroundColor={"bg-info"}/>
+                    <ColorCard title={"Warning"} subtitle={"#4e73df"} backgroundColor={"bg-warning"}/>
+                    <ColorCard title={"Danger"} subtitle={"#4e73df"} backgroundColor={"bg-danger"}/>
+                    <ColorCard title={"Secondary"} subtitle={"#4e73df"} backgroundColor={"bg-secondary"}/>
+                    <ColorCard title={"Dark"} subtitle={"#4e73df"} backgroundColor={"bg-dark"}/>
+                    <ColorCard title={"Light"} subtitle={"#4e73df"} backgroundColor={"bg-light"}
+                               foregroundColor={"text-muted"}/>
 
-                <Illustrations/>
-                <DevelopmentApproach/>
-            </div>
+                    <Illustrations/>
+                    <DevelopmentApproach/>
+                </div>
 
-            <Footer/>
+                <Footer/>
 
-            <div className={"fixed-bottom mb-4 me-4 text-end d-none"}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                     className="bi bi-arrow-up-circle text-muted" viewBox="0 0 16 16">
-                    <path fillRule="evenodd"
-                          d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
-                </svg>
+                <div className={"fixed-bottom mb-4 me-4 text-end d-none"}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                         className="bi bi-arrow-up-circle text-muted" viewBox="0 0 16 16">
+                        <path fillRule="evenodd"
+                              d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+                    </svg>
+                </div>
             </div>
         </div>
-    </div>
+    }
 }
 
-export default App;
