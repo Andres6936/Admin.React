751b358f305c561d02eb3c662163fdc2e743d58b
Joan Andrés <andres6936@live.com>
2021-03-02 20:08:37 -0500

    Added new item dropdown

--- src/App.js
+++ src/App.js
@@ -84,7 +84,8 @@ function App() {
                                     </div>
                                     <div className={"col-9"}>
                                         <p className={"small text-black-50 mb-0"}>December 12, 2019</p>
-                                        <p className={"small fw-bold text-wrap"}>A new monthly report is ready to
+                                        <p className={"small fw-bold text-muted text-wrap"}>A new monthly report is
+                                            ready to
                                             download!</p>
                                     </div>
                                 </div>
@@ -104,13 +105,31 @@ function App() {
                                     </div>
                                     <div className={"col-9"}>
                                         <p className={"small text-black-50 mb-0"}>December 7, 2019</p>
-                                        <p className={"small fw-bold text-wrap"}>$290.29 has been deposited into your
+                                        <p className={"small fw-bold text-muted text-wrap"}>$290.29 has been deposited
+                                            into your
                                             account!</p>
                                     </div>
                                 </div>
                             </a>
-                            <a className={"dropdown-item"}>
-
+                            <a className={"dropdown-item border-bottom mt-2"}>
+                                <div className={"row row-cols-2 d-flex align-items-center"}>
+                                    <div className={"col-3 text-center"}>
+                                        <div className={"rounded-circle bg-warning py-2 mx-1"}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                 fill="currentColor"
+                                                 className="bi bi-exclamation-triangle-fill text-white"
+                                                 viewBox="0 0 16 16">
+                                                <path
+                                                    d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className={"col-9"}>
+                                        <p className={"small text-black-50 mb-0"}>December 2, 2019</p>
+                                        <p className={"small fw-bold text-muted text-wrap"}>Spending Alert: We've
+                                            noticed unusually high spending for your account.</p>
+                                    </div>
+                                </div>
                             </a>
                             <a className={"dropdown-item"}>
 
