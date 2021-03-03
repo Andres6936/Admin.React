eaf44c149731a5e770f27bb0d89fa23a14a28530
Joan Andrés <andres6936@live.com>
2021-03-03 10:35:51 -0500

    Added new element to sidebar

--- src/Layout/Sidebar.js
+++ src/Layout/Sidebar.js
@@ -48,6 +48,17 @@ export class Sidebar extends React.Component {
                 </a>
 
                 <hr className={"text-white mx-3 my-1"}/>
+
+                <a className={"list-group-item"} style={{backgroundColor: "inherit", border: "none"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                         className="bi bi-table text-white" viewBox="0 0 16 16">
+                        <path
+                            d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 2h-4v3h4V4zm0 4h-4v3h4V8zm0 4h-4v3h3a1 1 0 0 0 1-1v-2zm-5 3v-3H6v3h4zm-5 0v-3H1v2a1 1 0 0 0 1 1h3zm-4-4h4V8H1v3zm0-4h4V4H1v3zm5-3v3h4V4H6zm4 4H6v3h4V8z"/>
+                    </svg>
+                    <p className={"text-white fw-bold mt-2 mb-0"} style={{fontSize: "10px"}}>Tables</p>
+                </a>
+
+                <hr className={"text-white mx-3 my-1"}/>
             </ul>
         </section>
     }
