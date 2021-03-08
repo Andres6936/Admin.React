8b1d49adf12342872d92bd70d5ec77f407469fb7
Joan Andrés <andres6936@live.com>
2021-03-08 12:57:51 -0500

    Deleted unused imports

--- src/__test__/Component/ProgressBar.test.js
+++ src/__test__/Component/ProgressBar.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from "enzyme-to-json";
-import {Row} from "../../Component/Row";
 import {ProgressBar} from "../../Component/ProgressBar";
 
 Enzyme.configure({adapter: new Adapter()})
