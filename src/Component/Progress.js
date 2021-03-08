547c8802b373bd3173e1608915f2abee14f44411
Joan Andrés <andres6936@live.com>
2021-03-08 13:46:30 -0500

    Added documentation Progress

--- src/Component/Progress.js
+++ src/Component/Progress.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+/**
+ * @param {string} colorBar (Required) The color of bar element.
+ * @param {number} percentage (Required) The percentage of this Progress bar. (Range: 0 to 100)
+ */
 export class Progress extends React.Component {
     render() {
         return (
